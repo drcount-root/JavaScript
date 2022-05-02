998dc52a935fd4464b6cad720011a815b2ea6cac
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-02 14:02:42 +0530

    02nd May 2022

--- [5]-Project1-Guess-The-Number/script.js
+++ [5]-Project1-Guess-The-Number/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
-console.log(document.querySelector(".message"));
+console.log(document.querySelector(".message").textContent);
+// for multiple dot operators they executes left to right direction
 // for id
 // console.log(document.querySelector("#message"));
