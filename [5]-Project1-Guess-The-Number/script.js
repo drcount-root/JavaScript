a20b5fe01f7d22e79c7d134ed9c2708f2b700619
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-02 23:39:31 +0530

    02nd May 2022

--- [5]-Project1-Guess-The-Number/script.js
+++ [5]-Project1-Guess-The-Number/script.js
@@ -17,3 +17,9 @@ console.log(document.querySelector(".message").textContent);
 
 document.querySelector(".message").textContent = "Correct Number 🎉";
 console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
