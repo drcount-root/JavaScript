d566c18cbd1c1874cc120c75e15f347f4ce9a14b
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-04 11:00:33 +0530

    04th May 2022

--- [5]-Project1-Guess-The-Number/script.js
+++ [5]-Project1-Guess-The-Number/script.js
@@ -1,25 +1,32 @@
 "use strict";
 
-// ----- getting elements -----
+// // ----- getting elements -----
 
-// for multiple dot operators they executes left to right direction
-// for class
-console.log(document.querySelector(".message").textContent);
-// for id
-// console.log(document.querySelector("#message").textContent);
-// for tag
-// console.log(document.querySelector("h1").textContent);
+// // for multiple dot operators they executes left to right direction
+// // for class
+// console.log(document.querySelector(".message").textContent);
+// // for id
+// // console.log(document.querySelector("#message").textContent);
+// // for tag
+// // console.log(document.querySelector("h1").textContent);
+// //
+// //
+// //
+// //
+// // ----- setting elements -----
+
+// document.querySelector(".message").textContent = "Correct Number 🎉";
+// console.log(document.querySelector(".message").textContent);
+
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
+
+// document.querySelector(".guess").value = 23;
+// console.log(document.querySelector(".guess").value);
 //
 //
 //
 //
-// ----- setting elements -----
-
-document.querySelector(".message").textContent = "Correct Number 🎉";
-console.log(document.querySelector(".message").textContent);
-
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
-
-document.querySelector(".guess").value = 23;
-console.log(document.querySelector(".guess").value);
+//
+//project
+document.querySelector(".check").addEventListener();
