d4c12cd433bb050d832d56e2e49243b081b2fce0
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-05 13:05:05 +0530

    05th May 2022

--- [5]-Project1-Guess-The-Number/script.js
+++ [5]-Project1-Guess-The-Number/script.js
@@ -41,6 +41,11 @@
 //
 //
 //project-code
+//
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
@@ -48,5 +53,25 @@ document.querySelector(".check").addEventListener("click", function () {
 
   if (!guess) {
     document.querySelector(".message").textContent = "No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Guess! 🎉";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "Game Over!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "Game Over!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
