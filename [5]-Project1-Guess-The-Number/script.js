7b671ed51c9197b1e0a826cb09c28bd4a5d0aaae
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-06 10:21:46 +0530

    06th May 2022

--- [5]-Project1-Guess-The-Number/script.js
+++ [5]-Project1-Guess-The-Number/script.js
@@ -42,10 +42,12 @@
 //
 //project-code
 //
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 // document.querySelector(".number").textContent = secretNumber;
-
+//
+//
+// Check! button functionalities
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
@@ -86,4 +88,18 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+//
+//
+//
+// Again button functionalities
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
