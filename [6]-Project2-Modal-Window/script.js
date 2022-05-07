3f3b272170d76efb0b06b88449e5167c5cc7983a
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-07 20:13:41 +0530

    07th May 2022

--- [6]-Project2-Modal-Window/script.js
+++ [6]-Project2-Modal-Window/script.js
@@ -1,7 +1,8 @@
 "use strict";
 
-const modal = document.querySelector(".show-modal");
+const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btn = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
+
 console.log(btnsOpenModal);
