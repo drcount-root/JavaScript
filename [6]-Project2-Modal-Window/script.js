73b8b09c5e733995c90fb4824d5d05c0194dc84c
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-07 20:43:26 +0530

    07th May 2022

--- [6]-Project2-Modal-Window/script.js
+++ [6]-Project2-Modal-Window/script.js
@@ -2,7 +2,23 @@
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btn = document.querySelector(".close-modal");
+const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-console.log(btnsOpenModal);
+const openModalFunc = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModalFunc = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModalFunc);
+}
+
+btnCloseModal.addEventListener("click", closeModalFunc);
+
+overlay.addEventListener("click", closeModalFunc);
