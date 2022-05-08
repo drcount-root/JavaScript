28f848b0f628fac51b613742150438cc2e8b30fc
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-08 13:39:21 +0530

    08th May 2022

--- [6]-Project2-Modal-Window/script.js
+++ [6]-Project2-Modal-Window/script.js
@@ -15,10 +15,21 @@ const closeModalFunc = function () {
   overlay.classList.add("hidden");
 };
 
+// open by clicking any of these buttons
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener("click", openModalFunc);
 }
 
+// close by clicking x sign
 btnCloseModal.addEventListener("click", closeModalFunc);
 
+// close by clicking outside
 overlay.addEventListener("click", closeModalFunc);
+
+// Close by pressing escape key
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModalFunc();
+  }
+});
