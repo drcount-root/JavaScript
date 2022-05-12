ec2a3fe34ef08ed48dc2f1999bb6245ceeee1a5a
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-12 07:44:27 +0530

    12th May 2022

---
+++ Efficient-JS-Programs/BASE-CLEAR-CODES/[5]_Hoisting/script.js
@@ -0,0 +1,21 @@
+console.log(c); // undefined
+var c = 5;
+
+// console.log(b); // Ref Error
+// let b = 10;
+
+// console.log(a); // Ref Error
+// const a = 20;
+
+// console.log(fnc1());
+// function fnc1() {
+//     return "Hi";
+// }
+// Returns Hi for function declaration
+
+// console.log(fnc2());
+// const fnc2 = function(){
+//     return "Hello";
+// }
+// RefError, fnc3() is not defined for function ecpression
+// Arrow function is also function expression
