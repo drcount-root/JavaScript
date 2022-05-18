af28d679ca797ee2fb0e3ed5ee65908373f69137
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-18 11:07:03 +0530

    c9

--- Initial-TestJS/script.js
+++ Initial-TestJS/script.js
@@ -7,3 +7,12 @@ parentDivElem.setAttribute("class", "parentDiv");
 parentDivElem.innerHTML = "<h1>Heading</h1><hr><p>Paragraph</p>";
 
 bodyElem[0].appendChild(parentDivElem);
+
+const a = document.createElement("p");
+a.innerText = "This";
+parentDivElem.appendChild(a);
+
+const ulList = document.createElement("ul");
+ulList.innerHTML = "<li>A</li><li>B</li><li>C</li>";
+parentDivElem.appendChild(ulList);
+ulList.setAttribute("class", "parentDiv");
