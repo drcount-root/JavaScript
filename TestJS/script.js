6334a5e0a04efa237f6e2b524a8a04cf89cfad74
Subham Sahu <about.subhamsahu@gmail.com>
2022-05-22 17:15:37 +0530

    cs

--- TestJS/script.js
+++ TestJS/script.js
@@ -1,4 +1,16 @@
 // Deleting/Removing a DOM Element
 
-const rem = document.getElementsByTagName('h1');
-rem[0].remove();
+// const rem = Array.from(document.getElementsByTagName('h1'));
+// rem[0].remove();
+
+// console.log(`Removed element : ${rem}`);
+
+// TACTICAL-THINGS remove() vs removeChild()
+// ref: MDN
+
+// remove()
+
+// creating something
+const body = document.getElementsByTagName('body');
+const parentDiv = document.createElement("div");
+parentDiv.
