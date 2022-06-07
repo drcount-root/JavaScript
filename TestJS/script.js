12e7ec24646eb3210ef7cb5c5acaeb3872354477
Subham Sahu <about.subhamsahu@gmail.com>
2022-06-07 08:19:10 +0530

    7cx-3

--- TestJS/script.js
+++ TestJS/script.js
@@ -7,3 +7,15 @@
 // Scope => Only in 'use strict' mode they are block scoped. And in 'sloppy mode' the func declarations are function scoped
 
 // 'use strict' came with ES5
+
+console.log(calcAge); // Initial value => the actual function
+
+console.log(calcAge(20)); // Accessible before initialization
+
+// They are stored inside the variable environment object, even before the code starts executing
+
+function calcAge(age) {
+  return age + 5;
+}
+
+// ***** So function declarations are hoisted.
