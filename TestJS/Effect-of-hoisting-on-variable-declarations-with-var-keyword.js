8eef498be04f593895e68d2006154a8e54c3bddc
Subham Sahu <about.subhamsahu@gmail.com>
2022-06-07 09:09:54 +0530

    7cx-6

--- TestJS/Effect-of-hoisting-on-variable-declarations-with-var-keyword.js
+++ TestJS/Effect-of-hoisting-on-variable-declarations-with-var-keyword.js
@@ -1,5 +1,9 @@
 'use strict';
 
+console.log(
+  '\n---------------- Effect of hoisting on variable declarations with var keyword ----------------'
+);
+
 // Effect of hoisting on var keyword
 
 // Variables declared with 'var' keyword are hoisted or not? => Yes, they're hoisted.
@@ -8,9 +12,13 @@
 
 // Scope => function scoped. For this code it's global scoped variable.
 
+console.log('\n---------------- Before initialization ----------------\n\n');
+
 console.log(age); // Before initialization we get 'undefined' instead of the assigned value later (20).
 
 var age = 20;
+
+console.log('\n---------------- After initialization ----------------\n\n');
 
 console.log(age); // We can get the value only when we log after variable declaration
 
